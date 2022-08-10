043931b0a451e16b9cf3cad9fab8b6fe7540817a
Adarsh <anitaalone1974@gmail.com>
2022-08-10 11:23:25 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const fileInput = document.querySelector('input'),
+ downloadBtn = document.querySelector('button');
+downloadBtn.addEventListener("click",e =>{
+    e.preventDefault();
+    downloadBtn.innerText = "downloading file....";
+fetchFile(fileInput.value);
+});
+
+function fetchFile(url){
+    fetch(url).then(res => res.blob()).then(file => {
+        let tempUrl = URL.createObjectURL(file);
+        const aTag = document.createElement("a");
+        aTag.href = tempUrl;
+        aTag.download = url.replace(/^.*[\\\/]/,'');
+        document.body.appendChild(aTag);
+        aTag.click();
+        downloadBtn.innerText = "Download File";
+        URL.revokeObjectURL(tempUrl);
+        aTag.remove();
+    }).catch(() =>{
+        alert("failed to download file!");
+        downloadBtn.innerText = "Download File";
+    });
+}
